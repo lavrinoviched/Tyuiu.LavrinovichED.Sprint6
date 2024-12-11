e77e4e7e534ebd93a46c4ab35146c767a944edaf
Tyuiu.lavrinoviched@outlook.com <Tyuiu.lavrinoviched@outlook.com>
2024-12-11 23:18:43 +0500

    Task7

--- Tyuiu.LavrinovichED.Sprint6.Task7.V23.Lib/DataService.cs
+++ Tyuiu.LavrinovichED.Sprint6.Task7.V23.Lib/DataService.cs
@@ -7,42 +7,36 @@ namespace Tyuiu.LavrinovichED.Sprint6.Task7.V23.Lib
     {
         public int[,] GetMatrix(string path)
         {
-
-            int[,] matrix = ReadMatrixFromFile(path);
-            ModifyLastColumn(matrix);
-            return matrix;
-        }
-
-        private int[,] ReadMatrixFromFile(string filePath)
-        {
-            string[] lines = File.ReadAllLines(filePath);
-            int rows = lines.Length;
-            int cols = lines[0].Split().Length;
-            int[,] matrix = new int[rows, cols];
-
-            for (int i = 0; i < rows; i++)
+            using (StreamReader reader = new StreamReader(path))
             {
-                string[] values = lines[i].Split();
-                for (int j = 0; j < cols; j++)
+                string[] lines = File.ReadAllLines(path);
+                int rows = lines.Length;
+                int cols = lines[0].Split().Length;
+                int[,] matrix = new int[rows, cols];
+
+                for (int i = 0; i < rows; i++)
                 {
-                    matrix[i, j] = int.Parse(values[j]);
+                    string[] values = lines[i].Split();
+                    for (int j = 0; j < cols; j++)
+                    {
+                        matrix[i, j] = int.Parse(values[j]);
+                    }
                 }
-            }
 
-            return matrix;
-        }
 
-        private void ModifyLastColumn(int[,] matrix)
-        {
-            int rows = matrix.GetLength(0);
-            int lastColIndex = matrix.GetLength(1) - 1;
 
-            for (int i = 0; i < rows; i++)
-            {
-                if (matrix[i, lastColIndex] < 2)
+                rows = matrix.GetLength(0);
+                int lastColIndex = matrix.GetLength(1) - 1;
+
+                for (int i = 0; i < rows; i++)
                 {
-                    matrix[i, lastColIndex] = 2;
+                    if (matrix[i, lastColIndex] < 2)
+                    {
+                        matrix[i, lastColIndex] = 2;
+                    }
                 }
+                return matrix;
+
             }
         }
 
